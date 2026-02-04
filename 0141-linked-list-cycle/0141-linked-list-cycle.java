c4917b0c3cae898f85aeebe69b73d87aedc8d3fa
Jebez Oswald <94307938+DevJebez@users.noreply.github.com>
2026-02-04 09:52:38 +0530

    Time: 8 ms (10.28%), Space: 46.5 MB (91.28%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        // Initialize a pointer 'temp'
+        // at the head of the linked list
+        ListNode temp = head;
+        int pos = -1;
+        // Create a map to keep track of
+        // encountered nodes
+        HashMap<ListNode, Integer> nodeMap = new HashMap<>();
+
+        // Step 2: Traverse the linked list
+        while (temp != null) {
+            // If the node is already in the
+            // map, there is a loop
+            if (nodeMap.containsKey(temp)) {
+                return true;
+            }
+            // Store the current node
+            // in the map
+            nodeMap.put(temp, pos);
+            // Move to the next node
+            temp = temp.next;
+        }
+
+        // Step 3: If the list is successfully traversed 
+        // without a loop, return false
+        return false;
+        }
+}
