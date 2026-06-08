5b60287b2b8250aae874c56a4f4cba9311469fdc
Jebez Oswald <94307938+DevJebez@users.noreply.github.com>
2026-06-08 11:40:41 +0530

    Time: 0 ms (100%), Space: 45 MB (22.7%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -7,7 +7,18 @@ class Solution {
         return Math.min(cost1, cost2);
     }
     */
+    static int minCost(int n, int[] dp, int[] cost){
+        if(n == 0 || n == 1) return 0;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = Math.min(minCost(n-1, dp, cost)+cost[n-1] , minCost(n-2, dp, cost)+cost[n-2]);
+        
+    }
     public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return minCost(n,dp, cost);
+        /*
         int n = cost.length;
         int[] dp = new int[n+1];
         dp[0] = 0;
@@ -18,5 +29,6 @@ class Solution {
             dp[i] = Math.min(cost1, cost2);
         }
         return dp[n];
+        */
     }
 }
