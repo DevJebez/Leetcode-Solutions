0896f870d60640fa09d8364b81675672169b6e75
Jebez Oswald <94307938+DevJebez@users.noreply.github.com>
2026-06-08 10:29:51 +0530

    Time: 0 ms (100%), Space: 42.2 MB (34.36%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -7,6 +7,7 @@ class Solution {
     }
     */
     public int climbStairs(int n) {
+        /*
         int[] dp = new int[n+1];
         dp[0] = 1;
         dp[1] = 1;
@@ -14,5 +15,16 @@ class Solution {
             dp[i] = dp[i-1] + dp[i-2];
         }
         return dp[n];
+        */
+
+        //space optimization
+        int prev1 = 1;
+        int prev2 = 1;
+        for(int i = 2; i <= n; i++){
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
     }
 }
