20957d82d4c88e03529ac97a94698503012a07ff
Jebez Oswald <94307938+DevJebez@users.noreply.github.com>
2026-06-11 21:31:44 +0530

    Time: 2 ms (16.13%), Space: 46.3 MB (81.91%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,23 +1,17 @@
 class Solution {
-    private int f(int i, int buy,int n, int[] arr, int[][] dp){
-        if(i == n) return 0;
-        if(dp[i][buy] != -1) return dp[i][buy];
-        int profit = 0;
-        if(buy == 0){
-            profit = Math.max(0+ f(i+1, 0, n, arr, dp),  (-1)*arr[i] + f(i+1, 1, n, arr, dp));
-        }
-        if(buy == 1){
-            profit = Math.max(0 + f(i+1, 1, n, arr, dp), arr[i] + f(i+1,0, n, arr, dp));
-        }
-        return dp[i][buy] = profit;
-    }
     public int maxProfit(int[] arr) {
         int n = arr.length;
-        int[][] dp = new int[n][2];
-        for(int[] row: dp){
-            Arrays.fill(row, -1);
+        int[][] dp = new int[n+1][2];
+        dp[0][1] = -arr[0]; // bought the first stock
+        dp[0][0] = 0; // didn't buy the first stock
+        for(int i = 1; i < n; i++){
+            //maximum of skipping and selling
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + arr[i]);
+            //maximum of holding and buying
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - arr[i]);
         }
-        return f(0, 0, n, arr, dp);
+        return dp[n-1][0];
+
     }
 }
 
