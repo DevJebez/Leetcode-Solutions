cf075b2029452022e5d42526b4839b4d75f4bfe8
Jebez Oswald <94307938+DevJebez@users.noreply.github.com>
2026-06-11 22:52:07 +0530

    Time: 56 ms (33.64%), Space: 117.8 MB (30.5%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,27 +1,21 @@
 class Solution {
-    private static int recurser(int i, int buy, int cap, int[] arr, int n, int[][][] dp){
-        if(i == n || cap == 0) return 0;
-        if(dp[i][buy][cap] != -1) return dp[i][buy][cap];
-        int profit = 0;
-        if(buy == 0){
-            profit = Math.max(recurser(i+1, 0, cap, arr, n,dp), recurser(i+1, 1, cap, arr, n, dp)-arr[i]);
-        }
-        if(buy == 1){
-            profit = Math.max(recurser(i+1, 1, cap, arr, n, dp), recurser(i+1, 0, cap-1, arr, n, dp)+arr[i]);
-        }
-        return dp[i][buy][cap] = profit;
-    }
     public int maxProfit(int[] arr) {
         int n = arr.length;
-        if(n == 0)  return 0;
-        int[][][] dp = new int[n][2][3];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < 2; j++){
-                Arrays.fill(dp[i][j], -1);
+        if(n == 0) return 0;
+        int[][][] dp = new int[n+1][2][3];
+        for(int i = n-1; i >= 0; i--){
+            for(int buy = 0; buy <= 1; buy++){
+                for(int cap = 1; cap <= 2; cap++){
+                    if(buy == 0){
+                        dp[i][buy][cap] = Math.max(dp[i+1][0][cap], dp[i+1][1][cap] - arr[i]);
+                    }
+                    if(buy == 1){
+                        dp[i][buy][cap] = Math.max(dp[i+1][1][cap], arr[i]+ dp[i+1][0][cap-1]);
+                    }
+                }
             }
         }
-        int ans = recurser(0, 0,2, arr, n, dp);
-        return ans;
+        return dp[0][0][2];
     }
 }
 
