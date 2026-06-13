2e07ffb46c6a7fa3bf543467b9aab837542f4eef
Jebez Oswald <94307938+DevJebez@users.noreply.github.com>
2026-06-13 06:21:20 +0530

    Time: 29 ms (30.27%), Space: 77.7 MB (24.19%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,15 +1,21 @@
 class Solution {
-  public int maxProfit(int[] arr,int fee) {
-    int n = arr.length;
-    int[][] dp = new int[n][2];
-    dp[0][0] = 0;
-    dp[0][1] = -arr[0];
-    int profit = 0;
-    for (int i = 1; i < n; i++) {
-      dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - arr[i]);
-      dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + arr[i] - fee);
+    private static int maxProfit(int i, int buy, int[] arr, int n, int fee, int[][] dp){
+        if(i == n) return 0;
+        if(dp[i][buy] != -1) return dp[i][buy];
+        int profit = 0;
+        if(buy == 0){
+            profit =  Math.max(maxProfit(i+1, 0, arr, n,fee, dp), maxProfit(i+1, 1, arr, n,fee, dp) - arr[i]);
+        }else{
+            profit = Math.max(maxProfit(i+1, 1, arr, n,fee, dp), maxProfit(i+1, 0, arr,n, fee, dp) + arr[i] - fee);
+        }
+        return dp[i][buy] = profit;
     }
-    //you should not return dp[n-1][1] because that is suboptimal and we can no longer buy asset on the last day.
-    return dp[n-1][0];
-  }
+    public int maxProfit(int[] arr,int fee) {
+        int n= arr.length;
+        int[][] dp = new int[n][2];
+        for(int i = 0; i < n;i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return maxProfit(0, 0, arr, n, fee, dp);
+    }
 }
